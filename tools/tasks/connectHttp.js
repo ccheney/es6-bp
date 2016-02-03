99426b6ee6003fbcec97a8025da2fdddca10cab4
rsavian <rsavian@nerdery.com>
2016-02-03 15:53:42 -0600

    Remove unnecessary files and npm packages.

--- tools/tasks/connectHttp.js
+++ tools/tasks/connectHttp.js
@@ -3,8 +3,6 @@
 
 module.exports = function(grunt) {
     var shouldOpen = grunt.option('open');
-    var path = require('path');
-    var mockApi = require('swagger-mock-api');
 
     grunt.config.merge({
         // Launches http-server
@@ -17,14 +15,6 @@ module.exports = function(grunt) {
                     base: '<%= env.DIR_DEST %>',
                     livereload: true,
                     open: shouldOpen ? true : false, // opens a tab in your default browser e.g. grunt launch --open
-                    //middleware: function(connect, options, middlewares) {
-                    //    middlewares.push(mockApi({
-                    //        swaggerFile: path.join(__dirname, '../testApi.yaml'),
-                    //        watch: true
-                    //    }));
-                    //
-                    //    return middlewares;
-                    //}
                 }
             }
         }
